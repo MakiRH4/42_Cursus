7974ae972894a556284b89836a30b15ed13feda9
Floris <floris@MacBook-Pro-von-Floris.local>
2023-11-01 14:51:27 +0100

    all

---
+++ ft_printf/printsf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
+
+int main(void)
+{
+    int i = 2;
+    printf ("this is a two %d\n", i);
+    printf ("mein funktion does dis\n");
+    ft_putstr_fd ("this is a two 2\n", 1);
+    
+    return (0);
+}
