dee86350190beeeb4bb8fafdeebe4aa1f4d7b91c
MakiRH4 <85067324+MakiRH4@users.noreply.github.com>
2023-11-02 02:02:59 +0100

    all_up

--- ft_printf/proof_of_concept.c
+++ ft_printf/proof_of_concept.c
@@ -15,10 +15,29 @@ void    ft_put_c(char c)
     ft_putchar_fd(c, 1);
 }
 
-int ft_printf(char const *text, ...);
+int ft_printf(char const *text, ...)
 {
     va_list arguments;
+    int i;
     
+    i = 0;
     va_start(arguments, text);
-    
+
+    while (text[i] != '%' && text[i] != '\0')
+    {
+        ft_putchar_fd(text[i], 1);
+        ++i;
+    }
+    if (text[i] == '%' && text[i + 1] != '\0' && text[i] == 'c')
+    {
+        char c = va_arg(arguments, char);
+        ft_putchar_fd(c, 1);
+    }
+    va_end(arguments);
+}
+
+int main(void)
+{
+    ft_printf("This is: %c", 'c');
+    return(0);
 }
