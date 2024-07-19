d4f071d754281033d38c3df95038a126e20fe49a
MakiRH4 <85067324+MakiRH4@users.noreply.github.com>
2024-07-19 14:52:27 +0200

    sync

---
+++ FdF/fdf.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fleonte <fleonte@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 22:59:47 by fleonte           #+#    #+#             */
+/*   Updated: 2024/07/18 22:59:47 by fleonte          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FDF_H
+# define FDF_H
+/*
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdint.h>
+
+typedef struct gnl_list
+{
+	char			*str_buff;
+	struct gnl_list	*link;
+}				t_gnl_list;
+*/
+void	fdf(type var);
+/*
+void		free_all(t_gnl_list **list, t_gnl_list *clean_node, char *buffer);
+void		copy_string(t_gnl_list *list, char *str);
+void		cp_str(t_gnl_list *gnl_list, char *str);
+void		append(t_gnl_list **list, char *buffer);
+void		build_list(t_gnl_list **list, int fd);
+void		polish_list(t_gnl_list **list);
+int			search_newline(t_gnl_list *gnl_list);
+int			len_line(t_gnl_list *gnl_list);
+char		*get_next_line(int fd);
+t_gnl_list	*get_last_node(t_gnl_list *gnl_list);
+*/
+#endif
