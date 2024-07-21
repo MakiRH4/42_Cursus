700331687038b87f62f25fdcbe801efd94c6b412
Floris Cristian Leonte <fleonte@c1r9s5.42madrid.com>
2024-07-21 23:24:15 +0200

    sync

--- config_backup/tweaks_config.sh
+++ config_backup/tweaks_config.sh
@@ -1,4 +1,4 @@
-xrandr --output eDP --mode 3200x1800 #sets screen resolution
+xrandr --output eDP --mode 2560x1440 #sets screen resolution
 xrandr --output eDP --brightness 0.90 #sets brightness
 ##xrandr --output eDP --scale 0.8x0.8
 
