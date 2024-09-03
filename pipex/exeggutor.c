5b00bc3c606309e74aa4730336131229bf464d7e
Floris Cristian Leonte <fleonte@c1r10s1.42madrid.com>
2024-09-03 02:16:34 +0200

    sync

---
+++ pipex/exeggutor.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exeggutor.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fleonte <fleonte@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 00:12:22 by fleonte           #+#    #+#             */
+/*   Updated: 2024/09/03 02:13:58 by fleonte          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+// executes the commands
+int	exeggutor(char *command_id, char **argv, char **env, int *piped_fds)
+{
+	pid_t	pid;
+	int		file_fd;
+
+	pid = fork();
+
+	if (pid < 0)
+		return (printf("fork for pid[0] failed"));
+	else if (pid == 0)
+	{
+		execve(find_path(command_id, env), &argv[2], env);
+//		execve(find_path(command_id, env), &argv, env); //doesn't work
+//		execve("/usr/bin/ls", "ls -l", env);
+//		exit;
+	}
+	return (pid); // Is it being returned if it is after an exit or an execve?
+}
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: fleonte <fleonte@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:17:20 by fleonte           #+#    #+#             */
-/*   Updated: 2024/09/02 02:45:37 by fleonte          ###   ########.fr       */
+/*   Updated: 2024/09/03 02:15:51 by fleonte          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # define COMMAND1		2	// 1st command
 # define COMMAND2		3	// 2nd command
 # define FILE2			4	// 2nd file
-# define PIPE_INLET		0
-# define PIPE_OUTLET	1
+# define READ			0
+# define WRITE			1
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -27,13 +27,13 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+int		exeggutor(char *command_id, char **argv, char **env, int *piped_fds);
 char	**ft_split(char const *s, char c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*find_path(char *command_id, char **env);
 char	*str_in_str(const char *haystack, const char *needle);
 size_t	ft_strlen(const char *s);
-int		pipex(char **command_id, char **argv, char **env);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 
